59e58ddd786590c383f929cb6ebe99c640397d2f
TheWerbinator <shawnthewerber@gmail.com>
2024-03-28 23:56:57 -0600

    Checkpoint 1

--- desktop/src/com/mygdx/imageeditor/DesktopLauncher.java
+++ desktop/src/com/mygdx/imageeditor/DesktopLauncher.java
@@ -10,6 +10,7 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("CS2810 Final");
+		config.setWindowedMode(584, 480);
 		System.out.println("Project made by: Shawn Werber");
 		new Lwjgl3Application(new ImageEditor(), config);
 	}
